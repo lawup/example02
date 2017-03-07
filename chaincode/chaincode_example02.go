9f2eb9daacf222d3bb8a79f3b323d9ca79ab26b4
素为 <lawup@qq.com>
2017-03-07 12:24:12 +0800

    Update chaincode_example02.go

--- chaincode/chaincode_example02.go
+++ chaincode/chaincode_example02.go
@@ -154,7 +154,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		return nil, errors.New(jsonResp0)
 	}
 	
-	//var valAsbytes string有大小限制
+	//var valAsbytes string有
 	
 	return valAsbytes0, nil
 }
